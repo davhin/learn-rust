0569351d3d56ef936ff1cb0b6450541df7e97911
David Hinrichs <dvd.hnrchs@gmail.com>
2025-04-18 23:59:55 +0200

    chapter 3

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,4 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 // fn main() {
 //     let mut x = 5;
@@ -20,31 +20,59 @@ const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 //     println!("The value of x is: {x}");
 // }
 
-#[derive(Debug)]
-struct Foo(&'static str);
-impl Drop for Foo {
-    fn drop(&mut self) {
-        println!("Dropping {:?}", self);
-    }
+// #[derive(Debug)]
+// struct Foo(&'static str);
+// impl Drop for Foo {
+//     fn drop(&mut self) {
+//         println!("Dropping {:?}", self);
+//     }
+// }
+
+// fn scopes() {
+//     // single‐scope shadowing
+//     let a = Foo("first");
+//     let a = Foo("second");
+//     println!("now a = {:?}", a);
+//     // neither Foo("first") nor Foo("second") are dropped here—
+//     // they’ll both be dropped at the end of `main` in reverse order:
+//     // first "second", then "first"
+
+//     // you cannot do `drop(a);` and then magically get back the "first" Foo,
+//     // because the name `a` was rebound to the "second" Foo and the "first" is hidden.
+
+//     // nested‐scope shadowing
+//     let b = Foo("outer");
+//     {
+//         let b = Foo("inner");
+//         println!("inside block, b = {:?}", b);
+//     } // here Foo("inner") is dropped
+//     println!("after block, b = {:?}", b); // refers to Foo("outer")
+// }
+
+
+use std::io;
+
+fn read_array() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
 
 fn main() {
-    // single‐scope shadowing
-    let a = Foo("first");
-    let a = Foo("second");
-    println!("now a = {:?}", a);
-    // neither Foo("first") nor Foo("second") are dropped here—
-    // they’ll both be dropped at the end of `main` in reverse order:
-    // first "second", then "first"
-
-    // you cannot do `drop(a);` and then magically get back the "first" Foo,
-    // because the name `a` was rebound to the "second" Foo and the "first" is hidden.
-
-    // nested‐scope shadowing
-    let b = Foo("outer");
-    {
-        let b = Foo("inner");
-        println!("inside block, b = {:?}", b);
-    } // here Foo("inner") is dropped
-    println!("after block, b = {:?}", b); // refers to Foo("outer")
+    read_array()
 }
