c790416e56cbbae64a6eb6443e56c69bc02e7d73
David Hinrichs <dvd.hnrchs@gmail.com>
2025-05-24 21:00:44 +0200

    start minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Looking for {query} in {file_path}");
+    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    println!("Full file: \n {contents}");
+}
