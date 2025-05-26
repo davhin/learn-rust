7ab6c573548edeaa3d7c716577f7fbc99e75aaf4
David Hinrichs <dvd.hnrchs@gmail.com>
2025-05-26 22:34:56 +0200

    read about closures

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,8 +5,11 @@ fn main() {
     println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
-    let file_path = &args[2];
+    let file_path: &String = &args[2];
     println!("Looking for {query} in {file_path}");
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect(
+        "should \
+    have been able to read the file",
+    );
     println!("Full file: \n {contents}");
 }
