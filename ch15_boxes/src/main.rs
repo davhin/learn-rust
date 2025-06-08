38828b535f2ff01d3556878a0ad3a93e118a1c13
David Hinrichs <dvd.hnrchs@gmail.com>
2025-06-08 21:28:46 +0200

    start ch16 concurrency

--- ch15_boxes/src/main.rs
+++ ch15_boxes/src/main.rs
@@ -1,5 +1,10 @@
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -20,6 +25,7 @@ impl<T> Deref for MyBox<T> {
     }
 }
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 struct CustomSmartPointer {
     data: String,
@@ -32,25 +38,36 @@ impl Drop for CustomSmartPointer {
 }
 
 fn main() {
-    let b = Box::new(5);
-    println!("Hello, box! b = {}", b);
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let b = Box::new(5);
+    // println!("Hello, box! b = {}", b);
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
-    // smart pointer deref
-    let x = 5;
-    let y = &x;
-    let z = MyBox::new(x);
+    // // smart pointer deref
+    // let x = 5;
+    // let y = &x;
+    // let z = MyBox::new(x);
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-    assert_eq!(5, *z);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+    // assert_eq!(5, *z);
 
-    // drop trait
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
+    // // drop trait
+    // let c = CustomSmartPointer {
+    //     data: String::from("my stuff"),
+    // };
+    // let d = CustomSmartPointer {
+    //     data: String::from("other stuff"),
+    // };
+    // println!("CustomSmartPointers created.");
+
+    // Rc<T>
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
