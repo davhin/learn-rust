a103cd1fde72d92a781e5619f4a65d1f668c23f5
David Hinrichs <dvd.hnrchs@gmail.com>
2025-06-09 15:27:51 +0200

    concurrency chapter

--- ch16_concurrency/src/main.rs
+++ ch16_concurrency/src/main.rs
@@ -1,3 +1,58 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+    handle.join().unwrap();
+
+    // move closures in threads
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || println!("here is a vector: {v:?}"));
+    handle.join().unwrap();
+
+    // use message to pass data between threads
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
